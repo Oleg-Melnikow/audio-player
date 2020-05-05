69f7a37977a72697a698623b3b3ea37a49d434aa
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2020-05-05 14:41:53 +0300

    Updates

---
+++ precache-manifest.6d76c13961d7e1e2859c2e5e92f7a7d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c6498a23648f08513058af995ca0f7",
+    "url": "/audio-player/index.html"
+  },
+  {
+    "revision": "b3e8e3b444e4606432ed",
+    "url": "/audio-player/static/css/main.9a65dd02.chunk.css"
+  },
+  {
+    "revision": "2f5e2927aed86967fc96",
+    "url": "/audio-player/static/js/2.848e466c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/audio-player/static/js/2.848e466c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e8e3b444e4606432ed",
+    "url": "/audio-player/static/js/main.c9aead7b.chunk.js"
+  },
+  {
+    "revision": "4f1e45bce8fc2e87b281",
+    "url": "/audio-player/static/js/runtime-main.69fe97dc.js"
+  },
+  {
+    "revision": "211dfe43379a26c54116f405caf3390f",
+    "url": "/audio-player/static/media/fromYesterday.211dfe43.jpg"
+  },
+  {
+    "revision": "4c5688606cea8ef1553f9f01bf765f9a",
+    "url": "/audio-player/static/media/fromyesterday.4c568860.mp3"
+  }
+]);
